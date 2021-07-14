894cf7c936626f140fec4cfba90e0f63a7c68078
junerae <80211043+junerae@users.noreply.github.com>
2021-07-14 15:43:59 +0200

    Add files via upload

---
+++ Opdracht11_whileAndForLoops/grade-assigner.js
@@ -0,0 +1,23 @@
+function assignGrade(score) {
+    if (score > 90) {
+        return "A";
+    } else if (score > 80) {
+        return "B";
+    } else if (score > 70) {
+        return "C";
+    } else if (score > 65) {
+        return "D";
+    } else {
+        return "E";
+    }
+}
+
+for (let i = assignGrade; i <= E; i++) {
+    // console.log(`Is ${i} even or odd?`);
+    if (i > 60, i < 100) {
+        console.log("For scoring 80 points, you get an C");
+    } else {
+        console.log("goodbye");
+    }
+}
+
